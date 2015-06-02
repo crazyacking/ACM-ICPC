c5faade965d658b29225812d4e903d4cb78f1ece
Devin <11301655@qq.com>
2015-06-02 13:04:34 +0800

    Update UVa 11498 - Division of Nlogonia.cpp

--- UVA/UVa-11498---Division-of-Nlogonia/src/UVa 11498 - Division of Nlogonia.cpp
+++ UVA/UVa-11498---Division-of-Nlogonia/src/UVa 11498 - Division of Nlogonia.cpp
@@ -12,7 +12,7 @@ int main() {
 			cin >> x >> y;
 
 			if (x == px || y == py)
-				cout << "divisa";
+				cout << "divisa";f
 			else if (x < px && y > py)
 				cout << "NO";
 			else if (x > px && y > py)
