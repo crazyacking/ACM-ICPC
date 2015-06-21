e6b51566aad68f4702cc1dac5bba898709364c49
Devin <11301655@qq.com>
2015-06-21 09:08:26 +0800

    Update poj 3259 .cpp

--- POJ/poj 3259 .cpp
+++ POJ/poj 3259 .cpp
@@ -8,7 +8,7 @@ int d[maxn],p[maxn],times[maxn],q[maxnmaxn];
 int m,n,wx,f,l,flag;
 void add(int x,int y,int z){
      to[++e]=y;
-     next[e]=begin[x];
+     next[e]=begin[x];；
      begin[x]=e;
      w[e]=z;
 }
