5fd9e3052609e701b1d60c658c0ac7e9cf807e72
crazyacking <11301655@qq.com>
2015-07-21 22:22:16 +0800

    Update

--- HDU/2015 Multi-University Training Contest 1 A/main.cpp
+++ HDU/2015 Multi-University Training Contest 1 A/main.cpp
@@ -1,93 +1,45 @@
 #include <iostream>
-#include <sstream>
-#include <iomanip>
-#include <fstream>
-#include <algorithm>
 #include <cstdio>
-#include <cmath>
-#include <string>
 #include <cstring>
 #include <vector>
-#include <queue>
-#include <utility>
 #include <set>
+#define ll long long
 using namespace std;
 
-#define mp make_pair
-#define fi first
-#define se second
-#define sz(a) (int)(a).size()
-#define rd(a) scanf("%d",&a)
-#define rdd(a,b) scanf("%d %d",&a,&b)
-#define rdt(a,b,c) scanf("%d %d %d",&a,&b,&c)
-#define ms(a,b) memset(a,b,sizeof a)
-#define pb push_back
-#define LD long double
-#define ll long long
-#define Maxn 0x7fffffff
-#define exp 1e-10
-#define lson (x<<1,l,mid)
-#define rson (x<<1|1,mid+1,r)
-
-typedef vector<int> vi;
+const int N = 100009;
 typedef pair<int, int> ii;
-typedef vector<ii> vii;
-typedef vector<string> vs;
-typedef vector<vi> vvi;
-const int INF = 200009;
-const int mod = ( int ) 1e9 + 7;
-vector<int> pos[10009],a[10009];
-int n;
-ll ans=0;
-void init(){
-      for(int i=1;i<=10000;++i) pos[i].clear();
-      ans=0;
-}
-
-void make(int x){
-      set<int> p;
-      set<int>::iterator it;
-      for(int i=0;i<sz(a[x]);++i){
-            for(int j=0;j<sz(pos[a[x][i]]);++j )
-            p.insert(pos[a[x][i]][j]);
-      }
-      for(int i=0;i<sz(pos[x]);++i){
-             it=p.find(pos[x][i]);
-             int a,b;
-             if(it==p.begin()) a=1;
-             else {
-                  --it;
-                  a=(*it)+1;
-                  ++it;
-             }
-             if((++it)==p.end()) b=n;
-             else{
-                  b=(*it)-1;
-             }
-             ans=(ans+(pos[x][i]-a+1)*(b-pos[x][i]+1))%mod;
-      }
-}
+set<ii> a, b;
+int g[N];
+int T, n, k;
+ll ans;
 int main()
 {
-      for(int i=1;i<=10000;++i){
-            for(int j=i;j<=10000;j+=i){
-                  a[j].pb(i);
-            }
-      }
-      while( scanf( "%d", &n ) != EOF )
+      scanf( "%d", &T );
+      while( T-- )
       {
-            init();
-            for(int i=1,k;i<=n;++i){
-                  scanf("%d",&k);
-                  //k=i%10000;
-                  pos[k].pb(i);
+            ans = 0;
+            a.clear();
+            b.clear();
+            scanf( "%d %d", &n, &k );
+            for( int i = 1; i <= n; ++i )
+            {
+                  scanf( "%d", &g[i] );
             }
-            for(int i=1;i<=10000;++i){
-                  if(sz(pos[i])){
-                        make(i);
+            int l = 1, r = 1;
+            while( r <= n )
+            {
+                  a.insert( make_pair( g[r], r ) );
+                  b.insert( make_pair( -g[r], r ) );
+                  while( - ( *b.begin() ).first  - ( *a.begin() ).first >= k )
+                  {
+                        a.erase( make_pair( g[l], l ) );
+                        b.erase( make_pair( -g[l], l ) );
+                        ++l;
                   }
+                  ans = ( ans + r - l + 1 );
+                  ++r;
             }
-            cout<<ans<<endl;
+            cout << ans << "\n";
       }
       return 0;
 }
--- HDU/2015 Multi-University Training Contest 1 B/main.cpp
+++ HDU/2015 Multi-University Training Contest 1 B/main.cpp
@@ -1,7 +1,7 @@
 /*
 * this code is made by crazyacking
 * Verdict: Accepted
-* Submission Date: 2015-07-21-19.00
+* Submission Date: 2015-07-21-21.38
 * Time: 0MS
 * Memory: 137KB
 */
@@ -24,13 +24,35 @@ using namespace std;
 
 int main()
 {
-      ios_base::sync_with_stdio(false);
-      cin.tie(0);
+      ios_base::sync_with_stdio( false );
+      cin.tie( 0 );
       int Cas;
-      cin>>Cas;
-      while(Cas--)
+      cin >> Cas;
+      while( Cas-- )
       {
-            int
+            int n, k, tmp;
+            cin >> n >> k;
+            vector<int> v;
+            for( int i = 0; i < n; ++i )
+            {
+                  cin >> tmp;
+                  v.push_back( tmp );
+            }
+            multiset<pair<int, int> > q1, q2;
+            int l = 0, r = 0;
+            LL ans = 0;
+            while( r < n )
+            {
+                  q1.insert( make_pair( v[r], r ) ), q2.insert( make_pair( -v[r], r ) );
+                  while( -( *q1.begin() ).first - ( *q2.begin() ).first >= k )
+                  {
+                        q1.erase( make_pair( v[l] , l ) ), q2.erase( make_pair( -v[l], l ) );
+                        l++;
+                  }
+                  ans += r - l + 1;
+                  r++;
+            }
+            cout << ans << endl;
       }
       return 0;
 }
