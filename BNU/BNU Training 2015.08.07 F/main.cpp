f133fa2fe0a94e39a8f8cff98e88237d864a3931
crazyacking <11301655@qq.com>
2015-08-08 09:55:14 +0800

    Update

--- BNU/BNU Training 2015.08.07 F/main.cpp
+++ BNU/BNU Training 2015.08.07 F/main.cpp
@@ -24,77 +24,58 @@ using namespace std;
 
 const int MAXN=1010;
 char s1[MAXN],s2[MAXN],ans[MAXN];
-int dp[MAXN][MAXN],path[MAXN][MAXN];
+int dp[MAXN][MAXN];
+char pa[MAXN][MAXN];
 
 void solve()
 {
-      memset(dp,0,sizeof dp);
-      memset(path,0,sizeof path);
       int cnt=0;
+      memset(dp,0,sizeof dp);
+      memset(pa,0,sizeof pa);
       int l1=strlen(s1),l2=strlen(s2);
       for(int i=0;i<l1;++i) dp[i][0]=0;
       for(int j=0;j<l2;++j) dp[0][j]=0;
       int enx=0,eny=0;
-      for(int i=0;i<l1;++i)
+      for(int i=1;i<l1;++i)
       {
-            for(int j=0;j<l2;++j)
+            for(int j=1;j<l2;++j)
             {
                   if(s1[i]==s2[j])
                   {
                         dp[i][j]=dp[i-1][j-1]+1;
-                        path[i][j]=1;
-                        enx=max(enx,i);
-                        eny=max(eny,j);
+                        pa[i][j]='*';
                   }
                   else
                   {
 
                         dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                        if(dp[i-1][j]>=dp[i][j-1]) path[i][j]=2;
-                        else path[i][j]=3;
+                        if(dp[i-1][j]>=dp[i][j-1]) pa[i][j]='s';
+                        else pa[i][j]='z';
                   }
             }
       }
-      ans[cnt++]=s1[enx];
-      cout<<enx<<" "<<eny<<endl;
-      while(enx>=0 && eny>=0)
+      enx=l1-1,eny=l2-1;
+      while(pa[enx][eny]!='\0')
       {
-            if(path[enx][eny]==1)
+            if(pa[enx][eny]=='*')
             {
-                  enx--;
-                  eny--;
-                  ans[cnt++]=s1[enx];
+                  enx--;eny--;
+                  ans[cnt++]=s1[enx+1];
             }
-            else if(path[enx][eny] ==2)
-            {
-                  enx--;
-            }
-            else if(path[enx][eny]==3) eny--;
+            else if(pa[enx][eny]=='s') enx--;
+            else if(pa[enx][eny]=='z') eny--;
       }
-      /**< 有问题,输出path看看 */
-      puts("- - - - - - - - - - - Program Run Here ! - - - - - - - - - - - - ");
-
-      for(int i=0;i<l1;++i)
-      {
-            for(int j=0;j<l2;++j)
-            {
-                  printf("%d",path[i][j]);
-            }
-            puts("");
-      }
-      puts("- - - - - - - - - - - Program Run Here ! - - - - - - - - - - - - ");
-
       ans[cnt]='\0';
       strrev(ans);
       puts(ans);
-      printf("%d\n",dp[l1-1][l2-1]);
 }
 
 int main()
 {
       ios_base::sync_with_stdio(false);
       cin.tie(0);
-      while(~scanf("%s%s",s1,s2))
+      s1[0]='$';s2[0]='$';
+      while(~scanf("%s%s",s1+1,s2+1))
       {
             solve();
       }
