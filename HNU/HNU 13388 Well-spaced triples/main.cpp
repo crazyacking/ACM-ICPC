137c85847f62945aa4a696604b98ad3238392064
crazyacking <11301655@qq.com>
2015-08-15 11:35:10 +0800

    Update

--- HNU/HNU 13388 Well-spaced triples/main.cpp
+++ HNU/HNU 13388 Well-spaced triples/main.cpp
@@ -1,7 +1,7 @@
 /*
 * this code is made by crazyacking
 * Verdict: Accepted
-* Submission Date: 2015-08-14-22.40
+* Submission Date: 2015-08-14-22.52
 * Time: 0MS
 * Memory: 137KB
 */
@@ -21,29 +21,28 @@
 #define  LL long long
 #define  ULL unsigned long long
 using namespace std;
-
+const int MAXN=10+(int)1e5;
+int n,j,gap;
+char s[MAXN];
 int main()
 {
       ios_base::sync_with_stdio(false);
       cin.tie(0);
-      int Cas;
-      cin>>Cas;
-      while(Cas--)
+      while(scanf("%d",&n),n!=-1)
       {
-            int n,x,y,z;
-            int a,b,c,get;
-            cin>>n>>x>>y>>z;
-            a=b=c=n;
-            LL ans=n;
-            while(a>=x || b>=y || c>=z)
+            scanf("%s",s+1);
+            int ans=0;
+            for(j=2;j<n;++j)
             {
-                  ans+=a/x+b/y+c/z;
-                  get=a/x+b/y+c/z;
-                  a=(a%x)+get;
-                  b=(b%y)+get;
-                  c=(c%z)+get;
+                  if(s[j]=='1')
+                  {
+                        for(gap=1;gap+j<=n&&j-gap>=1;++gap)
+                        {
+                              if(s[j-gap]=='1'&&s[gap+j]=='1') ans++;
+                        }
+                  }
             }
-            cout<<ans<<endl;
+            printf("%d\n",ans);
       }
       return 0;
 }
