5a8f8177cf90ae077b910ea8239867dcb60dfec5
crazyacking <11301655@qq.com>
2015-09-04 18:43:52 +0800

    Push up

--- other/Test1/main.cpp
+++ other/Test1/main.cpp
@@ -1,7 +1,7 @@
 /*
 * this code is made by crazyacking
 * Verdict: Accepted
-* Submission Date: 2015-09-02-15.40
+* Submission Date: 2015-09-03-12.21
 * Time: 0MS
 * Memory: 137KB
 */
@@ -23,84 +23,134 @@ typedef long long(LL);
 typedef unsigned long long(ULL);
 const double eps(1e-8);
 
-
-const int MAXN=100010;
-char s[MAXN],ss[110];
-
-
-//以下为倍增算法求后缀数组
-int wa[MAXN],wb[MAXN],wv[MAXN],Ws[MAXN];
-int cmp(int *r,int a,int b,int l)
-{return r[a]==r[b]&&r[a+l]==r[b+l];}
-/**< 传入参数：str,sa,len+1,ASCII_MAX+1 */
-void da(const char *r,int *sa,int n,int m)
+const int MAXN=10010,seed=131;
+int n,id[MAXN];
+char s[MAXN],str[1010];
+vector<pair<int,int> >same;
+bool isOk(int st,int en)
 {
-      int i,j,p,*x=wa,*y=wb,*t;
-      for(i=0; i<m; i++) Ws[i]=0;
-      for(i=0; i<n; i++) Ws[x[i]=r[i]]++;
-      for(i=1; i<m; i++) Ws[i]+=Ws[i-1];
-      for(i=n-1; i>=0; i--) sa[--Ws[x[i]]]=i;
-      for(j=1,p=1; p<n; j*=2,m=p)
+      int si=same.size();
+      for(int i=0; i<si; ++i)
       {
-            for(p=0,i=n-j; i<n; i++) y[p++]=i;
-            for(i=0; i<n; i++) if(sa[i]>=j) y[p++]=sa[i]-j;
-            for(i=0; i<n; i++) wv[i]=x[y[i]];
-            for(i=0; i<m; i++) Ws[i]=0;
-            for(i=0; i<n; i++) Ws[wv[i]]++;
-            for(i=1; i<m; i++) Ws[i]+=Ws[i-1];
-            for(i=n-1; i>=0; i--) sa[--Ws[wv[i]]]=y[i];
-            for(t=x,x=y,y=t,p=1,x[sa[0]]=0,i=1; i<n; i++)
-                  x[sa[i]]=cmp(y,sa[i-1],sa[i],j)?p-1:p++;
+            if(st>= (&same[i])->first && en<=(&same[i])->second)
+                  return (&same[i])->first;
       }
-      return;
-}
-int sa[MAXN],Rank[MAXN],height[MAXN];
-//求height数组
-/**< str,sa,len */
-void calheight(const char *r,int *sa,int n)
-{
-      int i,j,k=0;
-      for(i=1; i<=n; i++) Rank[sa[i]]=i;
-      for(i=0; i<n; height[Rank[i++]]=k)
-            for(k?k--:0,j=sa[Rank[i]-1]; r[i+k]==r[j+k]; k++);
-      // Unified
-      for(int i=n; i>=1; --i) ++sa[i],Rank[i]=Rank[i-1];
+      return -1;
 }
 
-char str[MAXN];
+vector<string> ans;
+bool judge(int p,bool ty)
+{
+      vector<pair<ULL,pair<int,int> > >v;
+      ULL hv=0,base=1;
+      for(int i=0; i<p; ++i)
+      {
+            hv=hv*seed+s[i];
+            base*=seed;
+      }
+      v.push_back(make_pair(hv,make_pair(0,p-1)));
+      int len=strlen(s);
+      for(int i=p; i<len; ++i)
+      {
+            hv=hv*seed+s[i] - base*s[i-p];
+            v.push_back(make_pair(hv,make_pair(i-p+1,i)));
+      }
+      sort(v.begin(),v.end());
+//      for(auto p2=v.begin();p2!=v.end();++p2)
+//      {
+//            int st=(&p2->second)->first;
+//            int en=(&p2->second)->second;
+//            int cnt=0;
+//            for(int i=st;i<=en;++i)
+//                  str[cnt++]=s[i];
+//            str[cnt]='\0';
+//            cout<<str<<endl;
+//      }
+      set<int> id;
+      int st,en,cnt;
+      bool f=false;
+      ans.clear();
+      auto p1=v.begin(),p2=v.begin();
+      ++p2;
+      for(; p2!=v.end(); ++p1,++p2)
+      {
+            if(p2->first != p1->first) id.clear();
+            else
+            {
+                  while(p2->first == p1->first)
+                  {
+                        st=(&p2->second)->first;
+                        en=(&p2->second)->second;
+                        if(isOk(st,en)!=-1 && id.find(isOk(st,en))==id.end() &&
+                                    isOk((&p1->second)->first,(&p1->second)->second)!=isOk(st,en))
+                        {
+                              id.insert(isOk(st,en));
+                        }
+                        ++p1,++p2;
+                  }
+                  --p1,--p2;
+                  if((id.size()+1)*2>n)
+                  {
+                        f=true;
+                        st=(&p2->second)->first;
+                        en=(&p2->second)->second;
+                        cnt=0;
+                        for(int i=st; i<=en; ++i)
+                              str[cnt++]=s[i];
+                        str[cnt]='\0';
+                        ans.push_back(str);
+                        id.clear();
+                  }
+            }
+      }
+      if(!ty) return f;
+      cout<<ans.size()<<endl;
+      sort(ans.begin(),ans.end());
+      for(int i=0; i<ans.size(); ++i)
+      {
+            cout<<ans[i]<<endl;
+      }
+}
+
+
 int main()
 {
-      while(scanf("%s",str)!=EOF)
+      ios_base::sync_with_stdio(false);
+      cin.tie(0);
+      while(scanf("%d",&n),n)
       {
-            int len=strlen(str);
-            da(str,sa,len+1,130);
-            calheight(str,sa,len);
-            puts("--------------All Suffix--------------");
-            for(int i=1; i<=len; ++i)
+            same.clear();
+            int maxLen=0,st,en;
+            s[0]='\0';
+            for(int i=0; i<n; ++i)
             {
-                  printf("%d:\t",i);
-                  for(int j=i-1; j<len; ++j)
-                        printf("%c",str[j]);
-                  puts("");
+                  scanf("%s",str);
+                  st=strlen(s);
+                  maxLen=max(maxLen,(int)strlen(str));
+                  strcat(s,str);
+                  en=strlen(s);
+                  same.push_back(make_pair(st,en-1));
             }
-            puts("");
-            puts("-------------After sort---------------");
-            for(int i=1; i<=len; ++i)
+            if(!judge(1,0))
             {
-                  printf("sa[%2d ] = %2d\t",i,sa[i]);
-                  for(int j=sa[i]-1; j<len; ++j)
-                        printf("%c",str[j]);
-                  puts("");
+                  puts("?");
+                  continue;
             }
-            puts("");
-            puts("---------------Height-----------------");
-            for(int i=1; i<=len; ++i)
-                  printf("height[%2d ]=%2d \n",i,height[i]);
-            puts("");
-            puts("----------------Rank------------------");
-            for(int i=1; i<=len; ++i)
-                  printf("Rank[%2d ] = %2d\n",i,Rank[i]);
-            puts("------------------END-----------------");
+            int l=1,r=maxLen,mid;
+            while(r>l+1)
+            {
+                  mid=l+(r-l)/2;
+                  if(judge(mid,0))
+                  {
+                        l=mid;
+                        cout<<l<<endl;
+                  }
+                  else r=mid;
+            }
+            judge(l,1);
       }
       return 0;
 }
+/*
+
+*/
--- other/Test2/main.cpp
+++ other/Test2/main.cpp
@@ -1,153 +1,37 @@
+/*
+* this code is made by crazyacking
+* Verdict: Accepted
+* Submission Date: 2015-09-04-17.26
+* Time: 0MS
+* Memory: 137KB
+*/
+#include <queue>
 #include <cstdio>
-#include <cstring>
 #include <set>
-#include <algorithm>
+#include <string>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <map>
+#include <cstdlib>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
 using namespace std;
-const int MAXLEN = 200005;
-
-struct Suffix_Arr
-{
-      int n, s[MAXLEN];
-      int tmp_one[MAXLEN], tmp_two[MAXLEN], c[MAXLEN];
-      int SA[MAXLEN], rank[MAXLEN], height[MAXLEN];
-
-      void init();
-      void build_arr(int m);
-      void get_height();
-} suf;
-
-int N, maxlen, id[MAXLEN];
-
-void init()
-{
-      maxlen = 0;
-      suf.init();
-      char word[1005];
-      for(int i = 0; i < N; i++)
-      {
-            scanf("%s", word);
-            if(N == 1)
-            {
-                  printf("%s\n", word);
-                  return;
-            }
-            int len = strlen(word);
-            maxlen = max(maxlen, len);
-            for(int j = 0; j < len; j++)
-            {
-                  id[suf.n] = i;
-                  suf.s[suf.n++] = word[j];
-            }
-            suf.s[suf.n++] = 'z' + i + 1;
-      }
-      suf.build_arr('z' + N + 1);
-      suf.get_height();
-}
-
-
-bool judge(int l, int flag)
-{
-      set<int> vis;
-      vis.insert(id[suf.SA[0]]);
-      for(int i = 1; i < suf.n; i++)
-      {
-            while(i < suf.n && suf.height[i] >= l)
-                  vis.insert(id[suf.SA[i++]]);
-            if(vis.size() * 2 > N)
-            {
-                  if(flag)
-                        return true;
-                  for(int j = 0; j < l; j++)
-                        printf("%c", suf.s[suf.SA[i-1] + j]);
-                  printf("\n");
-            }
-            vis.clear();
-            vis.insert(id[suf.SA[i]]);
-      }
-      return false;
-}
-
-void solve()
-{
-      if(N == 1)
-            return;
-      if(!judge(1, 1))
-      {
-            printf("?\n");
-            return;
-      }
-      int l = 1, r = maxlen + 1;
-      cout<<l<<" "<<r<<endl;
-      while(l < r)
-      {
-            int mid = (l + r) / 2;
-            if(judge(mid, 1))
-                  l = mid + 1;
-            else
-                  r = mid;
-      }
-      judge(l - 1, 0);
-}
+typedef long long(LL); 
+typedef unsigned long long(ULL);
+const double eps(1e-8);
 
 int main()
 {
-      int cas = 0;
-      while(scanf("%d", &N) == 1 && N)
-      {
-            if(cas++)
-                  printf("\n");
-            init();
-            solve();
-      }
+      ios_base::sync_with_stdio(false);
+      cin.tie(0);
+      int a,b;
+      cin>>a>>b;
+      cout<<a+b<<endl;
       return 0;
 }
+/*
 
-void Suffix_Arr::init()
-{
-      n = 0;
-}
-
-void Suffix_Arr::get_height()
-{
-      for(int i = 0; i < n; i++) rank[SA[i]] = i;
-      int k = 0;
-      for(int i = 0; i < n; i++)
-      {
-            if(k) k--;
-            if(rank[i] == 0)
-                  continue;
-            int j = SA[rank[i]-1];
-            while(s[i+k] == s[j+k]) k++;
-            height[rank[i]] = k;
-      }
-}
-
-void Suffix_Arr::build_arr(int m)
-{
-      int *x = tmp_one, *y = tmp_two;
-      for(int i = 0; i < m; i++) c[i] = 0;
-      for(int i = 0; i < n; i++) c[x[i] = s[i]]++;
-      for(int i = 1; i < m; i++) c[i] += c[i-1];
-      for(int i = n-1; i >= 0; i--) SA[--c[x[i]]] = i;
-      for(int k = 1; k <= n; k <<= 1)
-      {
-            int mv = 0;
-            for(int i = n - k; i < n; i++) y[mv++] = i;
-            for(int i = 0; i < n; i++) if(SA[i] >= k)
-                  {
-                        y[mv++] = SA[i] - k;
-                  }
-            for(int i = 0; i < m; i++) c[i] = 0;
-            for(int i = 0; i < n; i++) c[x[y[i]]]++;
-            for(int i = 1; i < m; i++) c[i] += c[i-1];
-            for(int i = n-1; i >= 0; i--) SA[--c[x[y[i]]]] = y[i];
-            swap(x, y);
-            mv = 1; x[SA[0]] = 0;
-            for(int i = 1; i < n; i++)
-                  x[SA[i]] = (y[SA[i-1]] == y[SA[i]] && y[SA[i-1] + k] == y[SA[i] + k] ? mv - 1 : mv++);
-            if(mv >= n)
-                  break;
-            m = mv;
-      }
-}
+*/
