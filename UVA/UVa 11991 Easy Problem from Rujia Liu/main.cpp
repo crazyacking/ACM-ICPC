c1657a7690c176c1ee952f949266ab0549abbd40
crazyacking <11301655@qq.com>
2015-09-06 14:10:27 +0800

    push up

--- UVA/UVa 11991 Easy Problem from Rujia Liu/main.cpp
+++ UVA/UVa 11991 Easy Problem from Rujia Liu/main.cpp
@@ -1,7 +1,7 @@
 /*
 * this code is made by crazyacking
 * Verdict: Accepted
-* Submission Date: 2015-09-06-11.47
+* Submission Date: 2015-09-06-12.20
 * Time: 0MS
 * Memory: 137KB
 */
@@ -22,12 +22,27 @@ using namespace std;
 typedef long long(LL);
 typedef unsigned long long(ULL);
 const double eps(1e-8);
-
+map<int,vector<int> > mp;
+// map<key,val> : key相当于下标map,即：mp[key]=val;
 int main()
 {
-      ios_base::sync_with_stdio(false);
-      cin.tie(0);
-      
+      int n,m,k,x;
+      while(~scanf("%d %d",&n,&m))
+      {
+            mp.clear();
+            for(int i=1;i<=n;++i)
+            {
+                  scanf("%d",&x);
+                  if(!mp.count(x)) mp[x]=vector<int>();
+                  mp[x].push_back(i);
+            }
+            while(m--)
+            {
+                  scanf("%d %d",&k,&x);
+                  if(!mp.count(x) || k>mp[x].size()) puts("0");
+                  else printf("%d\n",mp[x][k-1]);
+            }
+      }
       return 0;
 }
 /*
