89f957e98ebca404dd7bef0546e98b5346c89911
crazyacking <11301655@qq.com>
2015-09-06 11:52:25 +0800

    Push up

--- UVA/Uva 11020 Efficient Solutions/main.cpp
+++ UVA/Uva 11020 Efficient Solutions/main.cpp
@@ -1,7 +1,7 @@
 /*
 * this code is made by crazyacking
 * Verdict: Accepted
-* Submission Date: 2015-09-05-15.54
+* Submission Date: 2015-09-06-10.28
 * Time: 0MS
 * Memory: 137KB
 */
@@ -23,11 +23,43 @@ typedef long long(LL);
 typedef unsigned long long(ULL);
 const double eps(1e-8);
 
+struct point
+{
+      int x,y;
+      point(){}
+      point(int xx,int yy):x(xx),y(yy){}
+      bool operator <(const point& a) const
+      {
+            return x<a.x ||(x==a.x && y<a.y);
+      }
+};
 int main()
 {
       ios_base::sync_with_stdio(false);
       cin.tie(0);
-
+      int t,Cas;
+      scanf("%d",&t);
+      multiset<point> ms;
+      for(int Cas=1;Cas<=t;++Cas)
+      {
+            if(Cas>1) puts("");
+            printf("Case #%d:\n",Cas);
+            int n,x,y;
+            ms.clear();
+            scanf("%d",&n);
+            while(n--)
+            {
+                  scanf("%d %d",&x,&y);
+                  auto it=ms.lower_bound(point(x,y));
+                  if(it == ms.begin() || (--it)->y >y)
+                  {
+                        ms.insert(point(x,y));
+                        it=ms.upper_bound(point(x,y));
+                        while(it!=ms.end() && it->y>=y) ms.erase(it++);
+                  }
+                  printf("%d\n",ms.size());
+            }
+      }
       return 0;
 }
 /*
--- other/Test1/main.cpp
+++ other/Test1/main.cpp
@@ -1,19 +1,1 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<cmath>
 
-using namespace std;
-
-int main()
-{
-      int t,n;
-      scanf("%d",&t);
-      while(t--)
-      {
-            scanf("%d",&n);
-            double tmp=log10(double(n));
-            cout<<tmp<<endl;
-      }
-      return 0;
-}
