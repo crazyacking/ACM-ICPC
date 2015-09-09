1f6c16fd42af280aa0b4ee1be6efc32101741be2
crazyacking <11301655@qq.com>
2015-09-09 21:20:00 +0800

    Push up

--- other/Test1/main.cpp
+++ other/Test1/main.cpp
@@ -1,7 +1,7 @@
 /*
 * this code is made by crazyacking
 * Verdict: Accepted
-* Submission Date: 2015-09-09-13.48
+* Submission Date: 2015-09-09-16.41
 * Time: 0MS
 * Memory: 137KB
 */
@@ -23,21 +23,20 @@ using namespace std;
 typedef long long(LL);
 typedef unsigned long long(ULL);
 const double eps(1e-8);
-
 const int MAXN=100010;
-int n,m;
-int dp[100][100],gr[MAXN];
-int a[MAXN];
-vector<pair<int,int> > ve;
 
-void RMQ_init()
+int a[MAXN],dp[MAXN][30],f[MAXN],n,q;
+
+
+void RMQ_init(const vector<int>& A)
 {
-      for(int i=0;i<n;++i) dp[i][0]=a[i];
+      int n=A.size();
+      for(int i=0;i<n;++i) dp[i][0]=A[i];
       for(int j=1;(1<<j)<=n;++j)
       {
             for(int i=0;i+(1<<j)-1<n;++i)
             {
-                  dp[gr[i]][gr[j]]=max(ve[dp[gr[i]][gr[j-1]]].second,ve[dp[gr[i+(1<<(j-1))]][gr[j-1]]].second);
+                  dp[i][j]=min(dp[i][j-1],dp[i+(1<<(j-1))][j-1]);
             }
       }
 }
@@ -46,47 +45,40 @@ int RMQ(int L,int R)
 {
       int k=0;
       while((1<<(k+1))<=R-L+1) ++k;
-      return max(ve[dp[gr[L]][gr[k]]].second,ve[dp[gr[R-(1<<k)+1]][gr[k]]].second);
+      return min(dp[L][k],dp[R-(1<<k)+1][k]);
 }
+
 int main()
 {
+      freopen("C:\\Users\\crazyacking\\Desktop\\cin.txt","r",stdin);
+      //freopen("C:\\Users\\crazyacking\\Desktop\\cout.txt","w",stdout);
+
       ios_base::sync_with_stdio(false);
       cin.tie(0);
-      while(scanf("%d",&n),n!=0)
+      while(scanf("%d",&n),n)
       {
-            scanf("%d",&m);
-            ve.clear();
-            for(int i=0;i<n;++i)
-            {
-                  scanf("%d",&a[i]);
-            }
-            int num=0;
+            scanf("%d",&q);
+            vector<int> ve;
             for(int i=1;i<=n;++i)
+                  scanf("%d",&a[i]);
+            a[0]=a[1]-1;
+            int x=a[1];
+            ve.push_back(1);
+            for(int i=2;i<=n;++i)
             {
-                  gr[i-1]=ve.size();
-                  if(a[i]!=a[i-1])
-                  {
-                        ve.push_back(make_pair(a[i-1],num+1));
-                        num=0;
-                  }
-                  else ++num;
+                  if(x==a[i]) ve.push_back(ve[i-1]+1);
+                  else x=a[i],ve.push_back(1);
             }
-            //
-            for(int i=0;i<n;++i)
-            {
-                  printf("%2d ",gr[i]);
-            }
-            puts("");
-            auto p=ve.begin();
-            for(int i=0;i<ve.size();++i,++p)
-            {
-                  printf("%d %d %d\n",i,p->first,p->second);
-            }
-            while(m--)
+            RMQ_init(ve);
+            while(q--)
             {
                   int L,R;
                   scanf("%d %d",&L,&R);
-                  printf("%d\n",RMQ(L-1,R-1));
+                  --L,--R;
+                  int p=L;
+                  while(p<=R && f[p]==f[p-1]) ++p;
+                  int ans=RMQ(p,R);
+                  printf("%d\n",max(ans,p-L));
             }
       }
       return 0;
