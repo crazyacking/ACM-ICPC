ffb1be8ec954b2c73d4c7ec567b953a155c07afd
crazyacking <11301655@qq.com>
2015-09-19 10:09:20 +0800

    Push up

--- other/Test-1/main.cpp
+++ other/Test-1/main.cpp
@@ -1,60 +1,96 @@
-/*
-* this code is made by crazyacking
-* Verdict: Accepted
-* Submission Date: 2015-09-16-23.44
-* Time: 0MS
-* Memory: 137KB
-贪心
-*/
-#include <queue>
-#include <cstdio>
-#include <set>
-#include <string>
-#include <stack>
-#include <cmath>
-#include <climits>
-#include <map>
-#include <cstdlib>
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cstring>
-#define max(a,b) (a>b?a:b)
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long(LL);
-typedef unsigned long long(ULL);
-const double eps(1e-8);
+const int maxn = 1e5 + 131;
+typedef long long LL;
 
-long long a[400005];
-long long as[400005];
-long long n,m,k;
-long long ans_mx;
+LL Pow_Mod(LL a, LL b, LL p)
+{
+    LL ret = 1;
+    while(b)
+    {
+        if(b & 1) ret = (ret * a) % p;
+        a = a * a % p;
+        b >>= 1;
+    }
+    return ret;
+}
+
+void Exgcd(LL a, LL b, LL& d, LL& x, LL& y)
+{
+    if(b == 0) { d = a, x = 1, y = 0; }
+    else { Exgcd(b,a%b,d,y,x); y -= x * (a / b); }
+}
+///////////////////////////// Lucas
+LL Fac[maxn], Inv[maxn];
+
+void Init(LL n)
+{
+    Fac[0] = 1;
+    for(LL i = 1; i < n; ++i) Fac[i] = Fac[i-1] * i % n;
+    Inv[n-1] = Pow_Mod(Fac[n-1], n-2, n);
+    for(LL i = n-2; i >= 0; --i) Inv[i] = Inv[i+1] * (i + 1) % n;
+}
+
+LL C(LL m, LL n, LL p)
+{
+    if(n > m || m < 0 || n < 0) return 0;
+    return (Fac[m] * Inv[n]) % p * Inv[m-n] % p;
+}
+
+LL Lucas(LL m, LL n, LL p)
+{
+    if(n == 0) return 1;
+    return Lucas(m/p, n/p, p) * C(m%p, n%p, p) % p;
+}
+//////////////////////////////////
+LL Ai[maxn], Pi[maxn];
+
+LL mul(LL a, LL b, LL p)
+{
+    a = (a % p + p) % p;
+    b = (b % p + p) % p;
+    LL ret = 0;
+    while(b)
+    {
+        if(b & 1) ret = (ret + a)  % p;
+        b >>= 1;
+        a <<= 1;
+        a %= p;
+    }
+    return ret;
+}
+
+LL China(int n, LL *a, LL *m)
+{
+    LL x, y, d, M = 1;
+    LL ret = 0;
+    for(int i = 1; i <= n; ++i) M = M * m[i];
+    for(int i = 1; i <= n; ++i)
+    {
+        LL w = M / m[i];
+        /*  Wa  */
+//        y = Pow_Mod(w, m[i]-2, m[i]);
+        Exgcd(m[i],w,d,d,y) ;
+        ret = (ret + mul(a[i], mul(y, w, M), M)) % M;
+    }
+    return ret;
+}
+
 int main()
 {
-      scanf("%I64d %I64d %I64d",&n,&m,&k);
-      long long p=1;
-      long long i;
-      for(i=0; i<n; i++)
-      {
-            scanf("%I64d",&a[i]);
-      }
-      for(i=0; i<m; i++) p*=k;
-      for(i=0; i<n; i++) as[i]=a[i]*p;
-      long long item=0;
-      for(i=0; i<n; i++)
-      {
-            as[i]|=item;
-            item|=a[i];
-      }
-      item=0;
-      for(i=n-1; i>=0; i--)
-      {
-            as[i]=as[i]|item;
-            item=item|a[i];
-      }
-      ans_mx=0;
-      for(i=0; i<n; i++)
-            ans_mx=max(ans_mx,as[i]);
-      printf("%I64d\n",ans_mx);
-      return 0;
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        LL m, n; int k;
+        scanf("%lld%lld%d", &m, &n, &k);
+        for(int i = 1; i <= k; ++i)
+        {
+            scanf("%llu",&Pi[i]);
+            Init(Pi[i]);
+            Ai[i] = Lucas(m,n,Pi[i]);
+        }
+        LL ans = China(k,Ai,Pi);
+        printf("%lld\n",ans);
+    }
 }
--- other/Test-2/main.cpp
+++ other/Test-2/main.cpp
@@ -1,7 +1,7 @@
 /*
 * this code is made by crazyacking
 * Verdict: Accepted
-* Submission Date: 2015-09-17-01.42
+* Submission Date: 2015-09-19-10.07
 * Time: 0MS
 * Memory: 137KB
 */
@@ -20,28 +20,129 @@
 #include <cstring>
 #define max(a,b) (a>b?a:b)
 using namespace std;
-typedef long long(ll);
+typedef long long(LL);
 typedef unsigned long long(ULL);
 const double eps(1e-8);
 
-typedef pair<int,int> ii;
+/** O(n)内求出所有回文串
+ *原串 :abaaba
+ *Ma串 :.,a,b,a,a,b,a,
+ *Mp[i]:Ma串中，以字符Ma[i]为中心的最长回文子串的半径长度(包括Ma[i],也就是把回文串对折后的长度).
+ ****经过对原串扩展处理后，将奇数串的情况也合并到了偶数的情况(不需要考虑奇数串)
+ */
+const int MAXN=110010;
+char Ma[MAXN*2],s[MAXN];
+int Mp[MAXN*2],Mplen;
+void Manacher(char s[],int len)
+{
+      int le=0;
+      Ma[le++]='.';
+      Ma[le++]=',';
+      for(int i=0;i<len;++i)
+      {
+            Ma[le++]=s[i];
+            Ma[le++]=',';
+      }
+      Mplen=le;
+      Ma[le]=0;
+      int pnow=0,pid=0;
+      for(int i=1;i<le;++i)
+      {
+            if(pnow>i)
+                  Mp[i]=min(Mp[2*pid-i],pnow-i);
+            else
+                  Mp[i]=1;
+            for(;Ma[i-Mp[i]]==Ma[i+Mp[i]];++Mp[i]);
+            if(i+Mp[i]>pnow)
+            {
+                  pnow=i+Mp[i];
+                  pid=i;
+            }
+      }
+}
 
-const int MAXN=(int) 1e6+6;
-const int mod=(int)1e9+7;
+long long gcd(long long a,long long b)
+{
+      return !b?a:gcd(b,a%b);
+}
+long long gcd(long long a,long long b)
+{
+      return !b?a:gcd(b,a%b);
+}
 
-ll n,k,x,res;
-ll a[MAXN];
-ll cnt[MAXN];
-ll pre[MAXN],suf[MAXN];
-ll tr[MAXN];
-ll mx,mn,pos;
+long long lcm(long long a,long long b)
+{
+      return a*b/gcd(a,b);
+}
+/** Lucas
+ * p必须是小于100010的质数
+ */
+using namespace std;
+long long F[100010];
+void init(long long p)
+{
+      F[0] = 1;
+      for(int i = 1; i <= p; i++)
+            F[i] = F[i-1]*i%p;
+}
+long long inv(long long a,long long m)
+{
+      if(a == 1)return 1;
+      return inv(m%a,m)*(m-m/a)%m;
+}
+long long Lucas(long long n,long long m,long long p)
+{
+      long long ans = 1;
+      while(n&&m)
+      {
+            long long a = n%p;
+            long long b = m%p;
+            if(a < b)return 0;
+            ans = ans*F[a]%p*inv(F[b]*F[a-b]%p,p)%p;
+            n /= p;
+            m /= p;
+      }
+      return ans;
+}
 
-
+int main()
+{
+      int T;
+      int n,m,p;
+      scanf("%d",&T);
+      while(T--)
+      {
+            scanf("d%d",&n,&m,&p);
+            init(p);
+            printf("%d\n",(int)Lucas(n,m,p));
+      }
+     return 0;
+}
+|
 int main()
 {
       ios_base::sync_with_stdio(false);
       cin.tie(0);
-
+      while(~scanf("%s",s))
+      {
+            Manacher(s,strlen(s));
+            int ans=1,idx=0;
+            for(int i=0;i<Mplen;++i)
+            {
+                  if(Mp[i]>ans)
+                  {
+                        ans=Mp[i],idx=i;
+                  }
+            }
+            cout<<ans-1<<endl;
+      }
+      return 0;
+}
+int main()
+{
+      ios_base::sync_with_stdio(false);
+      cin.tie(0);
+      
       return 0;
 }
 /*
