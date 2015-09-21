b76686550ee2d40c3aa46b1eb9b25a57ee0e58c4
crazyacking <11301655@qq.com>
2015-09-21 21:35:33 +0800

    Push up

--- HDU/HDU 4291 A Short problem/main.cpp
+++ HDU/HDU 4291 A Short problem/main.cpp
@@ -41,12 +41,12 @@ Matrix mat_mul(Matrix a,Matrix b,LL M)
       Matrix ret;
       ret.n=a.n;
       ret.m=b.m;
-      for(int i=0;i<a.n;++i)
+      for(int i=0; i<a.n; ++i)
       {
-            for(int j=0;j<b.m;++j)
+            for(int j=0; j<b.m; ++j)
             {
                   ret.mat[i][j]=0;
-                  for(int k=0;k<a.m;++k)
+                  for(int k=0; k<a.m; ++k)
                   {
                         ret.mat[i][j]+=(a.mat[i][k]*b.mat[k][j]%M);
                         ret.mat[i][j]%=M;
@@ -62,9 +62,9 @@ Matrix mat_pow(Matrix a,LL n,LL M)
       if(n==1) return a;
       Matrix ret=a;
       Matrix tmp=a;
-      for(int i=0;i<a.n;++i)
+      for(int i=0; i<a.n; ++i)
       {
-            for(int j=0;j<a.n;++j)
+            for(int j=0; j<a.n; ++j)
             {
                   if(i==j) ret.mat[i][j]=1;
                   else ret.mat[i][j]=0;
@@ -89,13 +89,11 @@ int main()
       A.mat[0][1]=1;
       A.mat[1][0]=1;
       A.mat[1][1]=0;
-
       Matrix f0;
       f0.n=2;
       f0.m=1;
       f0.mat[0][0]=1;
       f0.mat[1][0]=0;
-
       Matrix tmp1,tmp2;
       LL n;
       while(~scanf("%I64d",&n))
@@ -103,19 +101,36 @@ int main()
             tmp1=mat_pow(A,n,MOD1);
             tmp2=mat_mul(tmp1,f0,MOD1);
             LL N=tmp2.mat[1][0];
-
             tmp1=mat_pow(A,N,MOD2);
             tmp2=mat_mul(tmp1,f0,MOD2);
             N=tmp2.mat[1][0];
-
             tmp1=mat_pow(A,N,MOD3);
             tmp2=mat_mul(tmp1,f0,MOD3);
             N=tmp2.mat[1][0];
-
             printf("%I64d\n",N);
       }
       return 0;
 }
 /*
+
+暴力求解循环节：
+
+const int MOD=1000000007;
+int main()
+{
+      int a=1,b=3,c;
+      for(int i=1;;++i)
+      {
+            if(a==0 && b==1)
+            {
+                  printf("%d\n",i);
+                  break;
+            }
+            c=(3*b+a)%MOD;
+            a=b;
+            b=c;
+      }
+      return 0;
+}
 
 */
---
+++ HIHO/ICPC Asia Regional Beijing Online B/2015 ACM/main.cpp
@@ -0,0 +1,36 @@
+/*
+* this code is made by crazyacking
+* Verdict: Accepted
+* Submission Date: 2015-09-21-21.04
+* Time: 0MS
+* Memory: 137KB
+*/
+#include <queue>
+#include <cstdio>
+#include <set>
+#include <string>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <map>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#define max(a,b) (a>b?a:b)
+using namespace std;
+typedef long long(LL);
+typedef unsigned long long(ULL);
+const double eps(1e-8);
+
+int main()
+{
+      ios_base::sync_with_stdio(false);
+      cin.tie(0);
+
+      return 0;
+}
+/*
+
+*/
