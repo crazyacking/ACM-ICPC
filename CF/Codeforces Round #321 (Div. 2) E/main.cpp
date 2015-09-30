ca64d6ba775135cdc44d898df112e3b1384367b8
crazyacking <11301655@qq.com>
2015-09-30 19:38:39 +0800

    Push up

--- CF/Codeforces Round #321 (Div. 2) E/main.cpp
+++ CF/Codeforces Round #321 (Div. 2) E/main.cpp
@@ -1,9 +1,36 @@
+/*
+* this code is made by crazyacking
+* Verdict: Accepted
+* Submission Date: 2015-09-30-19.37
+* Time: 0MS
+* Memory: 137KB
+*/
+#include <queue>
+#include <cstdio>
+#include <set>
+#include <string>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <map>
+#include <cstdlib>
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#define max(a,b) (a>b?a:b)
 using namespace std;
+typedef long long(LL);
+typedef unsigned long long(ULL);
+const double eps(1e-8);
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+      ios_base::sync_with_stdio(false);
+      cin.tie(0);
+
+      return 0;
 }
+/*
+
+*/
