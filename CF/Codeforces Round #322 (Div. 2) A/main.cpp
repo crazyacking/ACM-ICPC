634e867aa539bacfec3158ad5c2d9beba9c86f1d
crazyacking <11301655@qq.com>
2015-10-03 22:23:25 +0800

    Push up

--- CF/Codeforces Round #322 (Div. 2) A/main.cpp
+++ CF/Codeforces Round #322 (Div. 2) A/main.cpp
@@ -1,12 +1,40 @@
-#include<bits/stdc++.h>
+/*
+* this code is made by crazyacking
+* Verdict: Accepted
+* Submission Date: 2015-10-03-21.20
+* Time: 0MS
+* Memory: 137KB
+*/
+#include <queue>
+#include <cstdio>
+#include <set>
+#include <string>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <map>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#define max(a,b) (a>b?a:b)
 using namespace std;
+typedef long long(LL);
+typedef unsigned long long(ULL);
+const double eps(1e-8);
+
 int main()
 {
-      int a,b;
-      cin>>a>>b;
-      int ans1=min(a,b);
-      int big=max(a,b);
-      int ans2=(big-ans1)/2;
-      cout<<ans1<<" "<<ans2<<endl;
+      ios_base::sync_with_stdio(false);
+      cin.tie(0);
+      int n,m;
+      cin>>n>>m;
+      if(n>m) swap(n,m);
+      cout<<n<<" "<<(m-n)/2<<endl;
+
       return 0;
 }
+/*
+
+*/
