4c9482158e1db1c03ea0725afc04d6c36749ffbe
crazyacking <11301655@qq.com>
2015-10-31 10:52:35 +0800

    Push up

--- other/Test-1/main.cpp
+++ other/Test-1/main.cpp
@@ -1,7 +1,7 @@
 /*
 * this code is made by crazyacking
 * Verdict: Accepted
-* Submission Date: 2013-10-28-17.43
+* Submission Date: 2013-10-29-10.19
 * Time: 0MS
 * Memory: 137KB
 */
@@ -24,30 +24,43 @@ typedef long long(LL);
 typedef unsigned long long(ULL);
 const double eps(1e-8);
 
-const int N=110;
-const int M=10010;
-int w[N],v[N],dp[M];
-int n,m;
+const int MAXN=510;
+const int MAXM=10010;
+const int INF=(int)1e9+7;
+int dp[MAXM];
+int w[MAXN],v[MAXN];
 int main()
 {
       ios_base::sync_with_stdio(false);
       cin.tie(0);
-      while(cin>>n>>m)
+      int t;
+      cin>>t;
+      while(t--)
       {
-            for(int i=1;i<=n;++i)
-                  cin>>w[i]>>v[i];
-            for(int i=0;i<=m;++i)
-                  dp[i]=0;
-            for(int i=1;i<=n;++i)
+            int m,v1,v2;
+            cin>>v1>>v2;
+            m=v2-v1;
+            int n;
+            cin>>n;
+            for(int i=0;i<n;++i)
             {
-                  for(int j=m;j>=0;--j)
+                  cin>>v[i]>>w[i];
+            }
+            for(int i=0;i<=m;++i)
+                  dp[i]=INF;
+            dp[0]=0;
+            for(int i=0;i<n;++i)
+            {
+                  for(int j=w[i];j<=m;++j)
                   {
-                        if(j-w[i]<0)
-                              continue;
-                        dp[j]=max(dp[j],dp[j-w[i]]+v[i]);
+                        if(dp[j]>dp[j-w[i]]+v[i])
+                              dp[j]=dp[j-w[i]]+v[i];
                   }
             }
-            printf("%d\n",dp[m]);
+            if(dp[m]==INF)
+                  cout<<"This is impossible."<<endl;
+            else
+                  cout<<"The minimum amount of money in the piggy-bank is "<<dp[m]<<"."<<endl;
       }
       return 0;
 }
