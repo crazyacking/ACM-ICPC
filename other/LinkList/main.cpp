3b503a89a9745c2222e7d269df8351ed7f2e5cb6
crazyacking <11301655@qq.com>
2015-11-12 20:07:38 +0800

    Push up

---
+++ other/LinkList/main.cpp
@@ -0,0 +1,42 @@
+/*
+* -----------------------------------------------------------------
+* Copyright (c) 2015 crazyacking All rights reserved.
+* -----------------------------------------------------------------
+*       Author: crazyacking
+*       Verdict: Accepted
+*       Submission Date: 2015-11-11-23.40
+*/
+#include <queue>
+#include <cstdio>
+#include <set>
+#include <string>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <map>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#define max(a,b) (a>b?a:b)
+using namespace std;
+typedef long long(LL);
+typedef unsigned long long(ULL);
+const double eps(1e-8);
+
+/*
+假设以带头结点的循环链表表示队列，并且只设一个指针指向队尾元素站点（注意不设头指针）
+试编写相应的置空队、判队空、入队和出队等算法
+*/
+
+int main()
+{
+      ios_base::sync_with_stdio(false);
+      cin.tie(0);
+
+      return 0;
+}
+/*
+
+*/
