667dd2201ebc977bb034ae48786a86e20aefbc46
crazyacking <crazyacking@gmail.com>
2016-02-14 20:51:13 +0800

    要提交的变更： 	修改:         CF/Codeforces Round #319 (Div. 1)E/main.cpp

--- CF/Codeforces Round #319 (Div. 1)E/main.cpp
+++ CF/Codeforces Round #319 (Div. 1)E/main.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main()
 {
+    vector<int> ve;
+    int n;
+    while(cin>>n)
+    {
+        for(int i=0;i<n;++i)
+        {
+            int tmp;
+            cin>>tmp;
+            ve.push_back(tmp);
+        }
+        for(auto p : ve)
+        {
+            cout<<p<<endl;
+        }
+    }
     cout << "Hello world!" << endl;
     return 0;
 }
