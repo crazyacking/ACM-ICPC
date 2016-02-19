439844857880d9008e35733119e3a5454545a938
crazyacking <11301655@qq.com>
2016-02-19 19:34:24 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit. On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
    	new file:   LeetCode/29. Divide Two Integers/29. Divide Two Integers.depend
    	new file:   LeetCode/29. Divide Two Integers/29. Divide Two Integers.layout
    	modified:   LeetCode/29. Divide Two Integers/main.cpp
    	modified:   other/Test-1/Test-1.layout
    	modified:   other/Test-1/main.cpp

--- LeetCode/29. Divide Two Integers/main.cpp
+++ LeetCode/29. Divide Two Integers/main.cpp
@@ -1,9 +1,64 @@
+/**
+ * -----------------------------------------------------------------
+ * Copyright (c) 2016 crazyacking.All rights reserved.
+ * -----------------------------------------------------------------
+ *       Author: crazyacking
+ *       Date  : 2016-02-19-17.45
+ */
+#include <queue>
+#include <cstdio>
+#include <set>
+#include <string>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <map>
+#include <cstdlib>
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
+#include <cstring>
 using namespace std;
+typedef long long(LL);
+typedef unsigned long long(ULL);
+const double eps(1e-8);
+
+class Solution
+{
+public:
+    int divide(int dividend, int divisor)
+    {
+        if(!divisor || (dividend==INT_MIN && divisor==-1))
+            return INT_MAX;
+        bool sign=(dividend<0)^(divisor<0);
+        long long dvd=labs(dividend);
+        long long dvs=labs(divisor);
+        int res=0;
+        while(dvd>=dvs)
+        {
+            long long tmp=dvs,multiple=1;
+            while(dvd>=(tmp<<1))
+            {
+                tmp<<=1;
+                multiple<<=1;
+            }
+            dvd-=tmp;
+            res+=multiple;
+        }
+        return sign?-res:res;
+    }
+};
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Solution solution;
+    int a,b;
+    while(cin>>a>>b)
+    {
+        cout<<solution.divide(a,b)<<endl;
+    }
     return 0;
 }
+/*
+
+*/
--- other/Test-1/main.cpp
+++ other/Test-1/main.cpp
@@ -1,11 +1,25 @@
-#include<stdio.h>
-#include<string.h>
-int main()
+class Solution
 {
-    // 也可以改成 char str[] = "http://see.xidian.edu.cn/cpp/u/xitong/";
-    char *str = "http://see.xidian.edu.cn/cpp/u/xitong/";
-    char *substr = "see";
-    char *s = strstr(str, substr);
-    printf("%s\n", s);
-    return 0;
-}
+public:
+    int divide(int dividend, int divisor)
+    {
+        if (!divisor || (dividend == INT_MIN && divisor == -1))
+            return INT_MAX;
+        int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+        long long dvd = labs(dividend);
+        long long dvs = labs(divisor);
+        int res = 0;
+        while (dvd >= dvs)
+        {
+            long long temp = dvs, multiple = 1;
+            while (dvd >= (temp << 1))
+            {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            dvd -= temp;
+            res += multiple;
+        }
+        return sign == 1 ? res : -res;
+    }
+};
