a65f968d9a2a39d10adcdf6f172234099043f9c4
crazyacking <11301655@qq.com>
2016-02-19 08:42:48 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   LeetCode/22. Generate Parentheses/22. Generate Parentheses.layout
    	new file:   LeetCode/23. Merge k Sorted Lists/23. Merge k Sorted Lists.cbp
    	new file:   LeetCode/23. Merge k Sorted Lists/23. Merge k Sorted Lists.depend
    	new file:   LeetCode/23. Merge k Sorted Lists/23. Merge k Sorted Lists.layout
    	new file:   LeetCode/23. Merge k Sorted Lists/main.cpp

---
+++ LeetCode/23. Merge k Sorted Lists/main.cpp
@@ -0,0 +1,50 @@
+/**
+ * -----------------------------------------------------------------
+ * Copyright (c) 2016 crazyacking.All rights reserved.
+ * -----------------------------------------------------------------
+ *       Author: crazyacking
+ *       Date  : 2016-02-18-09.02
+ */
+#include <queue>
+#include <cstdio>
+#include <set>
+#include <string>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <map>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+typedef long long(LL);
+typedef unsigned long long(ULL);
+const double eps(1e-8);
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists)
+    {
+
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
+/*
+
+*/
