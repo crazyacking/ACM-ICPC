cf28771cde22200d66d015047a29415e7fd40d49
crazyacking <11301655@qq.com>
2016-03-14 19:02:51 +0800

    Changes to be committed: 	new file:   gziifapz.rfq.txt 	modified:   other/Test-1/Test-1.depend 	modified:   other/Test-1/Test-1.layout 	modified:   other/Test-1/main.cpp 	copied:     other/Test-1/main.cpp -> other/Test-2/main.cpp

--- other/Test-1/main.cpp
+++ other/Test-1/main.cpp
@@ -1,33 +1,100 @@
-/**
- * -----------------------------------------------------------------
- * Copyright (c) 2016 crazyacking.All rights reserved.
- * -----------------------------------------------------------------
- *       Author: crazyacking
- *       Date  : 2016-03-13-20.16
- */
-#include <queue>
-#include <cstdio>
-#include <set>
-#include <string>
-#include <stack>
-#include <cmath>
-#include <climits>
-#include <map>
-#include <cstdlib>
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cstring>
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long(LL);
-typedef unsigned long long(ULL);
-const double eps(1e-8);
+char input_file_dir[]="H:\\blog\\input.txt";
+char output_file_dir[]="H:\\blog\\output.txt";
+char s1[200];
+
+
+bool Is_first_title(char str[])
+{
+    int a[10][2]=
+    {
+        {-46,-69},
+        {-74,-2},
+        {-56,-3},
+        {-53,-60},
+        {-50,-27},
+        {-63,-7},
+        {-58,-33},
+        {-80,-53},
+        {-66,-59},
+        {-54,-82},
+    };
+    bool ret=false;
+    for(int i=0; i<10; ++i)
+    {
+        if(str[0]==a[i][0] && str[1]==a[i][1])
+        {
+            ret=true;
+            break;
+        }
+    }
+    return ret;
+}
+
+void The_end()
+{
+      puts("<p><span style=\"font-family: 'courier new', courier; font-size: 16px; line-height: 28px; color: #000000;\">");
+      printf("--------------------------------------------------------- End.");
+      puts("</span></p>");
+
+      puts("<p><span style=\"font-family: 'courier new', courier; font-size: 16px; line-height: 28px; color: #000000;\">");
+      printf("转载请注明：http://www.cnblogs.com/crazyacking/");
+      puts("</span></p>");
+}
 
 int main()
 {
-    cout<<sizeof(void*)<<endl;
+//      Is_first_title(s1);
+    freopen(input_file_dir,"r",stdin);
+    freopen(output_file_dir,"w",stdout);
+    int line_cnt=0;
+    int null_line=0;
+    while(gets(s1))
+    {
+        int type=0;
+        int len=strlen(s1);
+        int kong_ge_shu=0;
+        for(int i=0; i<len; ++i)
+        {
+            if(s1[i]==' ')
+                kong_ge_shu++;
+        }
+        if(len==0 || kong_ge_shu==len)
+        {
+            null_line++;
+            if(null_line>0)
+                continue;
+        }
+        else null_line=0;
+        for(int i=0; i<len; ++i)
+        {
+            if((i+1<len) && s1[i]==-95 && s1[i+1]==-94)  //、
+            {
+                s1[i]='.';
+                s1[i+1]=' ';
+            }
+
+            if((i+1<len) && s1[i]==-95 && s1[i+1]==-93) //。
+            {
+                s1[i]='.';
+                s1[i+1]='\n';
+            }
+        }
+        if(Is_first_title(s1))
+        {
+            puts("<p><strong><span style=\"font-family: 'Microsoft YaHei'; font-size: 14pt; \">");
+            printf("%s",s1);
+            puts("</span></strong></p>");
+        }
+        else
+        {
+//            puts("<p style=\"line-height: 2em;\"><span style=\"font-size: 18px; font-family: 楷体;\">&nbsp;&nbsp;&nbsp;&nbsp;<span style=\"font-family: 黑体; font-size: 16px;\">");
+            puts("<p><span style=\"font-family: 'courier new', courier; font-size: 16px; line-height: 28px; color: #000000;\">");
+            printf("%s",s1);
+            puts("</span></p>");
+        }
+    }
+    The_end();
     return 0;
 }
-/*
-
-*/
--- other/Test-2/main.cpp
+++ other/Test-2/main.cpp
@@ -3,7 +3,7 @@
  * Copyright (c) 2016 crazyacking.All rights reserved.
  * -----------------------------------------------------------------
  *       Author: crazyacking
- *       Date  : 2016-03-09-11.08
+ *       Date  : 2016-03-14-19.02
  */
 #include <queue>
 #include <cstdio>
@@ -23,89 +23,11 @@ typedef long long(LL);
 typedef unsigned long long(ULL);
 const double eps(1e-8);
 
-const int MAXN=10010;
-
-class Solution
-{
-public:
-    int find(int x)
-    {
-        return x!=parents[x]?parents[x]=find(parents[x]):x;
-    }
-
-    void init()
-    {
-        for(int i=0; i<MAXN; ++i)
-        {
-            parents[i]=i;
-            isLeaf[i]=true;
-        }
-    }
-
-    void create_relationship(int a,int b,int c)
-    {
-        isLeaf[a]=false;
-        parents[find(b)]=a;
-        parents[find(c)]=a;
-    }
-
-    void judge()
-    {
-        int x;
-        for(int i=0; i<MAXN; ++i)
-        {
-            x=find(i);
-            isLeaf[x]=false;
-        }
-    }
-
-    void query(int m)
-    {
-        for(int i=0; i<MAXN; ++i)
-        {
-            if(parents[i]==m && isLeaf[i])
-            {
-                cout<<i<<" ";
-            }
-        }
-        cout<<endl;
-        cout<<"-------------------"<<endl;
-    }
-
-private:
-    int parents[MAXN];
-    bool isLeaf[MAXN];
-
-};
-
 int main()
 {
-    int n;
-    Solution solution;
-    while(cin>>n)
-    {
-        solution.init();
-        int a,b,c; // 假设只有两个数依赖于a (多个数的话使用vector存储)
-        for(int i=0; i<n; ++i)
-        {
-            cin>>a>>b>>c;
-            solution.create_relationship(a,b,c);
-        }
-        solution.judge();
-        int m;
-        cin>>m;
-        solution.query(m);
-    }
+
     return 0;
 }
-
 /*
-测试数据：
-4
-1  2 3
-2  4 5
-3  6 7
-4  8 10
-1
 
 */
