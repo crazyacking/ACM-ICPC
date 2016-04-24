ca44c4ad2ad684de9730dfd2532b39172a26b23d
crazyacking <11301655@qq.com>
2016-04-25 01:17:50 +0800

    Changes to be committed: 	modified:   HDU/HDU 2640 - Toy bricks/main.cpp 	modified:   HDU/HDU 2640 - Toy bricks/test.cpp

--- HDU/HDU 2640 - Toy bricks/test.cpp
+++ HDU/HDU 2640 - Toy bricks/test.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 using namespace std;
 
-int t,n,m,dp[2][13][13],num,ok[13],cnt[13];
+int t, n, m, dp[2][13][13], num, ok[13], cnt[13];
 char map[105][10];
 
 /*
@@ -18,86 +18,86 @@ char map[105][10];
     00000010
     13种.
 */
-inline void init(){//求出合法状态
-    ok[0]=cnt[0]=0;
-    num=1;
-    for(int i=1;i+1<m;i++){
-        ok[num]=(1<<i),cnt[num]=1,num++;
-        for(int j=i+3;j+1<m;j++){
-            ok[num]=(1<<i)+(1<<j),cnt[num]=2,num++;
+inline void init() { //求出合法状态
+    ok[0] = cnt[0] = 0;
+    num = 1;
+    for (int i = 1; i + 1 < m; i++) {
+        ok[num] = (1 << i), cnt[num] = 1, num++; 
+        for (int j = i + 3; j + 1 < m; j++) {
+                    ok[num] = (1 << i) + (1 << j), cnt[num] = 2, num++; 
         }
     }
 }
 
-inline bool suit(int st,int i){//第i行状态为st,可以吗?需要考虑三行
-    for(int j=0;j<m;j++){
-        if(st&(1<<j)){
-            if(map[i][j]=='#' || map[i-1][j]=='#' || map[i-1][j-1]=='#' || map[i-1][j+1]=='#' || map[i-2][j]=='#')
+inline bool suit(int st, int i) { //第i行状态为st,可以吗?需要考虑三行
+    for (int j = 0; j < m; j++) {
+        if (st & (1 << j)) {
+            if (map[i][j] == '#' || map[i - 1][j] == '#' || map[i - 1][j - 1] == '#' || map[i - 1][j + 1] == '#' || map[i - 2][j] == '#')
                 return 0;
         }
     }
     return 1;
 }
 
-inline bool Can1(int st1,int st2){//对于物体,第三行的状态和第二行的状态合法不
+inline bool Can1(int st1, int st2) { //对于物体,第三行的状态和第二行的状态合法不
     int a[3][8];
-    memset(a,0,sizeof(a));
-    for(int i=0;i<8;i++){
-        if(st1&(1<<i)){
-            a[0][i]++,a[1][i]++,a[1][i-1]++,a[1][i+1]++,a[2][i]++;
+    memset(a, 0, sizeof(a));
+    for (int i = 0; i < 8; i++) {
+        if (st1 & (1 << i)) {
+            a[0][i]++, a[1][i]++, a[1][i - 1]++, a[1][i + 1]++, a[2][i]++;
         }
-        if(st2&(1<<i)){
-            a[1][i]++,a[2][i]++,a[2][i-1]++,a[2][i+1]++;
+        if (st2 & (1 << i)) {
+            a[1][i]++, a[2][i]++, a[2][i - 1]++, a[2][i + 1]++;
         }
     }
-    for(int i=0;i<8;i++){
-        if(a[0][i]>1 || a[1][i]>1 || a[2][i]>1) return 0;
+    for (int i = 0; i < 8; i++) {
+        if (a[0][i] > 1 || a[1][i] > 1 || a[2][i] > 1) return 0;
     }
     return 1;
 }
 
-inline bool Can2(int st1,int st2){//对于物体,第三行的状态和第一行的状态合法不
-    for(int i=0;i<m;i++){
-        if(st1&(1<<i)){
-            if(st2&(1<<i)){
+inline bool Can2(int st1, int st2) { //对于物体,第三行的状态和第一行的状态合法不
+    for (int i = 0; i < m; i++) {
+        if (st1 & (1 << i)) {
+            if (st2 & (1 << i)) {
                 return 0;
             }
         }
     }
     return 1;
 }
-int main(){
-    scanf("%d",&t);
-    while(t--){
-        scanf("%d%d",&n,&m);
-        for(int i=0;i<n;i++) scanf("%s",map[i]);
-        if(n<=2 || m<=2 ) puts("0");
-        else{
+int main() {
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d%d", &n, &m);
+        for (int i = 0; i < n; i++) scanf("%s", map[i]);
+        if (n <= 2 || m <= 2 ) puts("0");
+        else {
             init();
-            int now=0;
-            memset(dp[now],0,sizeof(dp[now]));
-            for(int i=2;i<n;i++){
-                now^=1;
-                memset(dp[now],0,sizeof(dp[now]));
-                for(int j=0;j<num;j++){
-                    int st1=ok[j];
-                    for(int k=0;k<num;k++){
-                        int st2=ok[k];
-                        for(int r=0;r<num;r++){
-                            int st3=ok[r];
-                            if(!suit(st3,i)) continue;
-                            if(!Can1(st3,st1)) continue;
-                            if(!Can2(st3,st2)) continue;
-                            dp[now][r][j]=max(dp[now][r][j],dp[1-now][j][k]+cnt[r]);
+            int now = 0;
+            memset(dp[now], 0, sizeof(dp[now]));
+            for (int i = 2; i < n; i++) {
+                now ^= 1;
+                memset(dp[now], 0, sizeof(dp[now]));
+                for (int j = 0; j < num; j++) {
+                    int st1 = ok[j];
+                    for (int k = 0; k < num; k++) {
+                        int st2 = ok[k];
+                        for (int r = 0; r < num; r++) {
+                            int st3 = ok[r];
+                            if (!suit(st3, i)) continue;
+                            if (!Can1(st3, st1)) continue;
+                            if (!Can2(st3, st2)) continue;
+                            dp[now][r][j] = max(dp[now][r][j], dp[1 - now][j][k] + cnt[r]);
                         }
                     }
                 }
             }
-            int ans=0;
-            for(int i=0;i<num;i++)
-                for(int j=0;j<num;j++)
-                    ans=max(ans,dp[now][i][j]);
-            printf("%d\n",ans);
+            int ans = 0;
+            for (int i = 0; i < num; i++)
+                for (int j = 0; j < num; j++)
+                    ans = max(ans, dp[now][i][j]);
+            printf("%d\n", ans);
         }
     }
     return 0;
